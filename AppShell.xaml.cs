d3c3b37a6876129b2fd2e811612996b4a5a413f8
patrisiatoth <patrisiatoth@gmail.com>
2024-11-18 19:01:00 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TothAntoniaPatrisiaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
