819127e1be2bcce0871d45e1190de253d32b066f
patrisiatoth <patrisiatoth@gmail.com>
2024-11-26 14:59:43 +0200

    laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,23 @@
-﻿namespace TothAntoniaPatrisiaLab7
+﻿using System;
+using TothAntoniaPatrisiaLab7.Data;
+using System.IO;
+
+namespace TothAntoniaPatrisiaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
